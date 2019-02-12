b7f87d2ebf5cb711d81ff15b23b0df292fe46387
Andy9FromSpace <andreas.wyss@gmail.com>
2019-02-12 23:30:32 +0100

    Fixed auto mapper type

--- Santolibre.Map.Elevation.WebService/AutoMapper.cs
+++ Santolibre.Map.Elevation.WebService/AutoMapper.cs
@@ -14,7 +14,7 @@ namespace Santolibre.Map.Elevation.WebService
             var config = new MapperConfiguration(x =>
             {
                 x.CreateMap<Lib.Models.SrtmRectangle, ApiControllers.v1.Models.SrtmRectangle>();
-                x.CreateMap<List<Lib.Models.Node>, ApiControllers.v1.Models.ElevationResponse>()
+                x.CreateMap<List<Lib.Models.INode>, ApiControllers.v1.Models.ElevationResponse>()
                     .ForMember(dest => dest.RangeHeight, opt => opt.MapFrom(src => src.Select(y => new float[] { (float)Math.Round(y.Distance * 1000), (float)Math.Round(y.Elevation, 1) }).ToList()));
             });
 
